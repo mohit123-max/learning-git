d0a0110f5f367e222a39a5f5cc29db02c7f0d921
mohit123-max <ms7920475@gmail.com>
2024-06-30 12:09:22 +0530

    added button in html and some css to it and dark mode functionality to it

---
+++ script.js
@@ -0,0 +1,14 @@
+let dark = false;
+
+document.querySelector("button").onclick = ()=>{
+    if(dark === false){
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        dark = true ; 
+    }
+    else{
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        dark = false ; 
+    }
+}
